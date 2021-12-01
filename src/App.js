cc0ad0298a97e1220e328581868cf0eadadaf635
Tasin <tasinsiddiqi@yahoo.com>
2021-12-01 15:53:10 +0100

    all texts added

--- src/App.js
+++ src/App.js
@@ -79,34 +79,227 @@ const App = () => {
       <button className="Ressourcen" onMouseEnter={() => {
         if (app===1){
           setText({
-          text: "Resources text1",
-          heading: "reosurce heading"
+          text: "Zur Herstellung von Produkten werden verschiedene Materialien und Rohstoffe verwendet. Dabei wird zwischen erneuerbaren und nicht erneuerbaren sowie primären und sekundären Rohstoffen unterschieden. Rohstoffe wie Metall und Glas können mit geringen Qualitätsverlusten recycelt und somit als Sekundärrohstoff verwendet werden.",
+          heading: "Rohstoffe"
         })}
         else{
           setText({
-            text: "Resources text2",
-            heading: "reosurce heading"
+            text: "Zur Herstellung von Produkten werden verschiedene Materialien und Rohstoffe verwendet. Dabei wird zwischen erneuerbaren und nicht erneuerbaren sowie primären und sekundären Rohstoffen unterschieden. Rohstoffe wie Metall und Glas können mit geringen Qualitätsverlusten recycelt und somit als Sekundärrohstoff verwendet werden.",
+            heading: "Rohstoffe"
           })
         }
         
       }} 
       onMouseLeave={() => {
-        setText(text)
+        setText(textmain)
       }}
       onClick={() => {
         
         setTextFixed({
-          text: "Resources text",
-          heading: "reosurce heading"
+          text: "Zur Herstellung von Produkten werden verschiedene Materialien und Rohstoffe verwendet. Dabei wird zwischen erneuerbaren und nicht erneuerbaren sowie primären und sekundären Rohstoffen unterschieden. Rohstoffe wie Metall und Glas können mit geringen Qualitätsverlusten recycelt und somit als Sekundärrohstoff verwendet werden.",
+          heading: "Rohstoffe"
 
         })
-      }}><img src={Ressourcen} alt="Ressourcen" className="Ressourcen"/></button>
-      <button className="Design"><img src="./Design.png" alt="Design"/></button>
-      <button className="Produktion"><img src="./Produktion.png" alt="Produktion"/></button>
-      <button className="Vertrieb"><img src="./Vertrieb.png" alt="Vertrieb"/></button>
-      <button className="Nutzung"><img src="./Nutzung.png" alt="Nutzung"/></button>
-      <button className="Nutzungsende"><img src="./Nutzungsende.png" alt="Nutzungsende"/></button>
-      <button className="Entsorgung"><img src="./Entsorgung.png" alt="Entsorgung"/></button>
+      }}></button>
+
+      <button className="Design" onMouseEnter={() => {
+        if (app===1){
+          setText({
+          text: "Das Produktdesign hat einen maßgeblichen Einfluß auf alle vorgelagerten und nachgelagerten Prozesse. In dieser Phase entscheidet sich beispielsweise, ob ein Produkt für eine lange Nutzung oder als Einwegprodukt konzipiert wird. Materialwahl, Verpackung, Produktion, zeitgemäßes oder zeitloses Design und viele weitere Parameter beeinflussen schon in der Designphase den weiteren Lebensweg des Produkts. Ein Produktdesigner hat viele Anforderungen zu erfüllen und viele Parameter zu beachten. Ein Ansatz Produkte und Prozesse nachhaltig zu gestalten ist EcoDesign. [Mehr Dazu]",
+          heading: "Design"
+        })}
+        else{
+          setText({
+            text: "Eco Design ist ein Gestaltungsansatz für Produkte, mit dem Ziel Umweltbelastungen über den gesamten Produktlebenszyklus zu mindern. Durch verbesserte Produktgestaltung können Produzenten in der Produktplanungs- und -designphase einen positiven Einfluss auf den gesamten Lebensweg des Produktes nehmen. Eco Design ist ein systematischer und umfassender Gestaltungsansatz, der verschiedene Prinzipien und Methodiken bereitstellt, an denen sich Produzenten orientieren können.",
+            heading: " Eco Design"
+          })
+        }
+        
+      }} 
+      onMouseLeave={() => {
+        setText(textmain)
+      }}
+      onClick={() => {
+        if (app===1){
+          setTextFixed({
+            text: "Das Produktdesign hat einen maßgeblichen Einfluß auf alle vorgelagerten und nachgelagerten Prozesse. In dieser Phase entscheidet sich beispielsweise, ob ein Produkt für eine lange Nutzung oder als Einwegprodukt konzipiert wird. Materialwahl, Verpackung, Produktion, zeitgemäßes oder zeitloses Design und viele weitere Parameter beeinflussen schon in der Designphase den weiteren Lebensweg des Produkts. Ein Produktdesigner hat viele Anforderungen zu erfüllen und viele Parameter zu beachten. Ein Ansatz Produkte und Prozesse nachhaltig zu gestalten ist EcoDesign. [Mehr Dazu]",
+            heading: "Design"
+  
+          })
+        }
+        else{
+        setTextFixed({
+          text: "Eco Design ist ein Gestaltungsansatz für Produkte, mit dem Ziel Umweltbelastungen über den gesamten Produktlebenszyklus zu mindern. Durch verbesserte Produktgestaltung können Produzenten in der Produktplanungs- und -designphase einen positiven Einfluss auf den gesamten Lebensweg des Produktes nehmen. Eco Design ist ein systematischer und umfassender Gestaltungsansatz, der verschiedene Prinzipien und Methodiken bereitstellt, an denen sich Produzenten orientieren können.",
+          heading: "Eco Design"
+
+        })
+      }
+      }}></button>
+
+      <button className="Produktion" onMouseEnter={() => {
+        if (app===1){
+          setText({
+          text: "Je nach Funktion und Form des Produktes, eignen sich verschiedene Herstellungsverfahren. Bei der Wahl des geeigneten Herstellungsverfahren, sollten die Ressourcenverbräuche verglichen werden. Additive Herstellungsmethoden, wie der 3D Druck sind durch die geringen Nebenprodukte aus Umweltsicht attraktiv.",
+          heading: "Produktion"
+        })}
+        else{
+          setText({
+            text: "Je nach Funktion und Form des Produktes, eignen sich verschiedene Herstellungsverfahren. Bei der Wahl des geeigneten Herstellungsverfahren, sollten die Ressourcenverbräuche verglichen werden. Additive Herstellungsmethoden, wie der 3D Druck sind durch die geringen Nebenprodukte aus Umweltsicht attraktiv.",
+            heading: "Produktion"
+          })
+        }
+        
+      }} 
+      onMouseLeave={() => {
+        setText(textmain)
+      }}
+      onClick={() => {
+        if (app===1){
+          setTextFixed({
+            text: "Je nach Funktion und Form des Produktes, eignen sich verschiedene Herstellungsverfahren. Bei der Wahl des geeigneten Herstellungsverfahren, sollten die Ressourcenverbräuche verglichen werden. Additive Herstellungsmethoden, wie der 3D Druck sind durch die geringen Nebenprodukte aus Umweltsicht attraktiv.",
+            heading: "Produktion"
+  
+          })
+        }
+        else{
+        setTextFixed({
+          text: "Je nach Funktion und Form des Produktes, eignen sich verschiedene Herstellungsverfahren. Bei der Wahl des geeigneten Herstellungsverfahren, sollten die Ressourcenverbräuche verglichen werden. Additive Herstellungsmethoden, wie der 3D Druck sind durch die geringen Nebenprodukte aus Umweltsicht attraktiv.",
+          heading: "Produktion"
+        })
+      }
+      }}></button>
+
+      <button className="Vertrieb" onMouseEnter={() => {
+        if (app===1){
+          setText({
+          text: "In linearen Wirtschaftssystemen konzertriert sich der Vertrieb vor allem darauf Waren und Dienstleistungen zum Kunden zu bringen. Dazu wird, je nach Produkt, ein entsprechendes Logistiknetz genutzt oder aufgebaut. In der Kreislaufwirtschaft erweitern sich die Aufgaben des Vertriebs. Der Kunde wird stärker eingebunden, Rücknahmesysteme werden etabliert und neue Services und Vertriebskanäle entstehen, die dem Kunden eine umfassendere User Experience bieten. Moderne, innovative Vertriebswege bieten großes Potential Kunden zu binden und die steigende Nachfrage nach nachhaltigen Produkten und Dienstleistungen zu befriedigen.",
+          heading: "Vertrieb"
+        })}
+        else{
+          setText({
+            text: "In linearen Wirtschaftssystemen konzertriert sich der Vertrieb vor allem darauf Waren und Dienstleistungen zum Kunden zu bringen. Dazu wird, je nach Produkt, ein entsprechendes Logistiknetz genutzt oder aufgebaut. In der Kreislaufwirtschaft erweitern sich die Aufgaben des Vertriebs. Der Kunde wird stärker eingebunden, Rücknahmesysteme werden etabliert und neue Services und Vertriebskanäle entstehen, die dem Kunden eine umfassendere User Experience bieten. Moderne, innovative Vertriebswege bieten großes Potential Kunden zu binden und die steigende Nachfrage nach nachhaltigen Produkten und Dienstleistungen zu befriedigen.",
+            heading: "Vertrieb"
+          })
+        }
+        
+      }} 
+      onMouseLeave={() => {
+        setText(textmain)
+      }}
+      onClick={() => {
+        if (app===1){
+          setTextFixed({
+            text: "In linearen Wirtschaftssystemen konzertriert sich der Vertrieb vor allem darauf Waren und Dienstleistungen zum Kunden zu bringen. Dazu wird, je nach Produkt, ein entsprechendes Logistiknetz genutzt oder aufgebaut. In der Kreislaufwirtschaft erweitern sich die Aufgaben des Vertriebs. Der Kunde wird stärker eingebunden, Rücknahmesysteme werden etabliert und neue Services und Vertriebskanäle entstehen, die dem Kunden eine umfassendere User Experience bieten. Moderne, innovative Vertriebswege bieten großes Potential Kunden zu binden und die steigende Nachfrage nach nachhaltigen Produkten und Dienstleistungen zu befriedigen.",
+            heading: "Vertrieb"
+  
+          })
+        }
+        else{
+        setTextFixed({
+          text: "In linearen Wirtschaftssystemen konzertriert sich der Vertrieb vor allem darauf Waren und Dienstleistungen zum Kunden zu bringen. Dazu wird, je nach Produkt, ein entsprechendes Logistiknetz genutzt oder aufgebaut. In der Kreislaufwirtschaft erweitern sich die Aufgaben des Vertriebs. Der Kunde wird stärker eingebunden, Rücknahmesysteme werden etabliert und neue Services und Vertriebskanäle entstehen, die dem Kunden eine umfassendere User Experience bieten. Moderne, innovative Vertriebswege bieten großes Potential Kunden zu binden und die steigende Nachfrage nach nachhaltigen Produkten und Dienstleistungen zu befriedigen.",
+          heading: "Vertrieb"
+        })
+      }
+      }}></button>
+
+      <button className="Nutzung" onMouseEnter={() => {
+        if (app===1){
+          setText({
+          text: "In vielen Anwendungen wird der Umwelteinfluss der Nutzungsphase unterschätzt. Für die meisten Produkte gilt jedoch, je länger die Produkte genutzt werden, umso geringer der Materialaufwand und damit der Ressourcenverbrauch. Im Hinblick auf die Kreislaufwirtschaft bedeutet das, dass mit einer Verlängerung der Nutzungsphase die Neuproduktion von Gütern verringert wird aber sich der Servicebedarf erhöht. Somit werden Dienstleistungspotenziale geschaffen, die Raum für innovative Serviceleistungen bieten. Kunden werden gebunden und Ressourcenintensive Industrien minimiert.",
+          heading: "Nutzung"
+        })}
+        else{
+          setText({
+            text: "In vielen Anwendungen wird der Umwelteinfluss der Nutzungsphase unterschätzt. Für die meisten Produkte gilt jedoch, je länger die Produkte genutzt werden, umso geringer der Materialaufwand und damit der Ressourcenverbrauch. Im Hinblick auf die Kreislaufwirtschaft bedeutet das, dass mit einer Verlängerung der Nutzungsphase die Neuproduktion von Gütern verringert wird aber sich der Servicebedarf erhöht. Somit werden Dienstleistungspotenziale geschaffen, die Raum für innovative Serviceleistungen bieten. Kunden werden gebunden und Ressourcenintensive Industrien minimiert.",
+            heading: "Nutzung"
+          })
+        }
+        
+      }} 
+      onMouseLeave={() => {
+        setText(textmain)
+      }}
+      onClick={() => {
+        if (app===1){
+          setTextFixed({
+            text: "In vielen Anwendungen wird der Umwelteinfluss der Nutzungsphase unterschätzt. Für die meisten Produkte gilt jedoch, je länger die Produkte genutzt werden, umso geringer der Materialaufwand und damit der Ressourcenverbrauch. Im Hinblick auf die Kreislaufwirtschaft bedeutet das, dass mit einer Verlängerung der Nutzungsphase die Neuproduktion von Gütern verringert wird aber sich der Servicebedarf erhöht. Somit werden Dienstleistungspotenziale geschaffen, die Raum für innovative Serviceleistungen bieten. Kunden werden gebunden und Ressourcenintensive Industrien minimiert.",
+            heading: "Nutzung"
+  
+          })
+        }
+        else{
+        setTextFixed({
+          text: "In vielen Anwendungen wird der Umwelteinfluss der Nutzungsphase unterschätzt. Für die meisten Produkte gilt jedoch, je länger die Produkte genutzt werden, umso geringer der Materialaufwand und damit der Ressourcenverbrauch. Im Hinblick auf die Kreislaufwirtschaft bedeutet das, dass mit einer Verlängerung der Nutzungsphase die Neuproduktion von Gütern verringert wird aber sich der Servicebedarf erhöht. Somit werden Dienstleistungspotenziale geschaffen, die Raum für innovative Serviceleistungen bieten. Kunden werden gebunden und Ressourcenintensive Industrien minimiert.",
+          heading: "Nutzung"
+        })
+      }
+      }}></button>
+
+      <button className="Nutzungsende" onMouseEnter={() => {
+        if (app===1){
+          setText({
+          text: "Das Nutzungsende eines Produktes ist dann erreicht, wenn es von niemanden weitergenutzt wird. Das heißt, wenn es weder vom Erstnutzer noch von einem anderen Nutzer weiterverwendet wird. Dies kann beispielsweise bei einem nicht reparierbaren Defekt eines Produktes der Fall sein, bei dem das Produkt nicht mehr den gewünschten Zweck erfüllt. Ein anderer Grund für das Nutzungsende kann die Ablösung des Produktes durch ein moderneres sein, beispielsweise wenn es nicht mehr dem aktuellen Trend entspricht. Durch die Gestaltung von ästhetisch zeitlosen oder modularen Produkten kann dieser Grund für das Nutzungsende hinausgezögert werden.",
+          heading: "Nutzungsende"
+        })}
+        else{
+          setText({
+            text: "Das Nutzungsende eines Produktes ist dann erreicht, wenn es von niemanden weitergenutzt wird. Das heißt, wenn es weder vom Erstnutzer noch von einem anderen Nutzer weiterverwendet wird. Dies kann beispielsweise bei einem nicht reparierbaren Defekt eines Produktes der Fall sein, bei dem das Produkt nicht mehr den gewünschten Zweck erfüllt. Ein anderer Grund für das Nutzungsende kann die Ablösung des Produktes durch ein moderneres sein, beispielsweise wenn es nicht mehr dem aktuellen Trend entspricht. Durch die Gestaltung von ästhetisch zeitlosen oder modularen Produkten kann dieser Grund für das Nutzungsende hinausgezögert werden.",
+            heading: "Nutzungsende"
+          })
+        }
+        
+      }} 
+      onMouseLeave={() => {
+        setText(textmain)
+      }}
+      onClick={() => {
+        if (app===1){
+          setTextFixed({
+            text: "Das Nutzungsende eines Produktes ist dann erreicht, wenn es von niemanden weitergenutzt wird. Das heißt, wenn es weder vom Erstnutzer noch von einem anderen Nutzer weiterverwendet wird. Dies kann beispielsweise bei einem nicht reparierbaren Defekt eines Produktes der Fall sein, bei dem das Produkt nicht mehr den gewünschten Zweck erfüllt. Ein anderer Grund für das Nutzungsende kann die Ablösung des Produktes durch ein moderneres sein, beispielsweise wenn es nicht mehr dem aktuellen Trend entspricht. Durch die Gestaltung von ästhetisch zeitlosen oder modularen Produkten kann dieser Grund für das Nutzungsende hinausgezögert werden.",
+            heading: "Nutzungsende"
+  
+          })
+        }
+        else{
+        setTextFixed({
+          text: "Das Nutzungsende eines Produktes ist dann erreicht, wenn es von niemanden weitergenutzt wird. Das heißt, wenn es weder vom Erstnutzer noch von einem anderen Nutzer weiterverwendet wird. Dies kann beispielsweise bei einem nicht reparierbaren Defekt eines Produktes der Fall sein, bei dem das Produkt nicht mehr den gewünschten Zweck erfüllt. Ein anderer Grund für das Nutzungsende kann die Ablösung des Produktes durch ein moderneres sein, beispielsweise wenn es nicht mehr dem aktuellen Trend entspricht. Durch die Gestaltung von ästhetisch zeitlosen oder modularen Produkten kann dieser Grund für das Nutzungsende hinausgezögert werden.",
+          heading: "Nutzungsende"
+        })
+      }
+      }}></button>
+
+      <button className="Entsorgung" onMouseEnter={() => {
+        if (app===1){
+          setText({
+          text: "Nach dem Nutzungsende eines Produktes, wird dieses, sofern nicht einzelne Komponenten oder Materialien wiederverwendet oder recycelt werden, entsorgt. In Bezug auf die Abfallwirtschaft ist das Ziel der Kreislaufwirtschaft die stoffliche Verwertung nicht vermeidbarer Abfälle. Weiterhin sollen Innovation dazu beitragen die Abfallwirtschaft zu einer Quelle für die Beschaffung von Rohstoffen für die Produktion von Gütern weiterzuentwickeln. Nicht verwertbare Abfälle müssen bevor sie in Deponien abgelagert oder thermisch verwertet werden, aufwendig mechanisch, biologisch oder thermisch behandelt werden, wodurch weitere Umweltwirkungen und Kosten entstehen.",
+          heading: "Entsorgung"
+        })}
+        else{
+          setText({
+            text: "Nach dem Nutzungsende eines Produktes, wird dieses, sofern nicht einzelne Komponenten oder Materialien wiederverwendet oder recycelt werden, entsorgt. In Bezug auf die Abfallwirtschaft ist das Ziel der Kreislaufwirtschaft die stoffliche Verwertung nicht vermeidbarer Abfälle. Weiterhin sollen Innovation dazu beitragen die Abfallwirtschaft zu einer Quelle für die Beschaffung von Rohstoffen für die Produktion von Gütern weiterzuentwickeln. Nicht verwertbare Abfälle müssen bevor sie in Deponien abgelagert oder thermisch verwertet werden, aufwendig mechanisch, biologisch oder thermisch behandelt werden, wodurch weitere Umweltwirkungen und Kosten entstehen.",
+            heading: "Entsorgung"
+          })
+        }
+        
+      }} 
+      onMouseLeave={() => {
+        setText(textmain)
+      }}
+      onClick={() => {
+        if (app===1){
+          setTextFixed({
+            text: "Nach dem Nutzungsende eines Produktes, wird dieses, sofern nicht einzelne Komponenten oder Materialien wiederverwendet oder recycelt werden, entsorgt. In Bezug auf die Abfallwirtschaft ist das Ziel der Kreislaufwirtschaft die stoffliche Verwertung nicht vermeidbarer Abfälle. Weiterhin sollen Innovation dazu beitragen die Abfallwirtschaft zu einer Quelle für die Beschaffung von Rohstoffen für die Produktion von Gütern weiterzuentwickeln. Nicht verwertbare Abfälle müssen bevor sie in Deponien abgelagert oder thermisch verwertet werden, aufwendig mechanisch, biologisch oder thermisch behandelt werden, wodurch weitere Umweltwirkungen und Kosten entstehen.",
+            heading: "Entsorgung"
+  
+          })
+        }
+        else{
+        setTextFixed({
+          text: "Nach dem Nutzungsende eines Produktes, wird dieses, sofern nicht einzelne Komponenten oder Materialien wiederverwendet oder recycelt werden, entsorgt. In Bezug auf die Abfallwirtschaft ist das Ziel der Kreislaufwirtschaft die stoffliche Verwertung nicht vermeidbarer Abfälle. Weiterhin sollen Innovation dazu beitragen die Abfallwirtschaft zu einer Quelle für die Beschaffung von Rohstoffen für die Produktion von Gütern weiterzuentwickeln. Nicht verwertbare Abfälle müssen bevor sie in Deponien abgelagert oder thermisch verwertet werden, aufwendig mechanisch, biologisch oder thermisch behandelt werden, wodurch weitere Umweltwirkungen und Kosten entstehen.",
+          heading: "Entsorgung"
+        })
+      }
+      }}></button>
     </>
   );
 }
